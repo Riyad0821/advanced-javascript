dc5a68a7ce68bac3d7c22ce00bde1f7b91b56a92
Riyad0821 <mdriyad0821@gmail.com>
2020-08-09 15:54:36 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//Falsy
+//0
+//""
+//undefined
+//null
+//NaN
+//false
+//Truthy
+//'0', ' ', [], {}
+
+//const name = " ";
+let name = [];
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}

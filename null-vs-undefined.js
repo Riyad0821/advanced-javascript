46b22597547fdd77d81cb42e2d84cc5c98cc4385
Riyad0821 <mdriyad0821@gmail.com>
2020-08-09 16:14:24 +0600

    null-vs-undefined cleared for exam

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+//Undefined
+//let value;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const riyad = {name: "Mohammad Riyad", phone: "01795463762"};
+// console.log(riyad.address);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [2, 4, 6];
+console.log(ages[11]);

02d78a1bdbe79e55b5803735d33f58950ad169be
Riyad0821 <mdriyad0821@gmail.com>
2020-08-09 16:30:52 +0600

    double-vs-triple-equal cleard for exam

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,17 @@
+// const first = 0;
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
